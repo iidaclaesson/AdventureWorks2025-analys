4a09fd6714f26da81396a86163b590ac4fe1d2b4
iidaclaesson <iidaclaesson@gmail.com>
2025-12-31 13:50:14 +0100

    code for vis4

--- data/vis4.sql
+++ data/vis4.sql
@@ -3,3 +3,13 @@ USE AdventureWorks2025;
 
 SELECT * FROM Sales.SalesOrderHeader
 
+
+
+SELECT
+    SUM(TotalDue) AS TotalOrderSales,
+    COUNT(SalesOrderID) AS OrderCount,
+    YEAR(OrderDate) AS OrderYear
+FROM Sales.SalesOrderHeader
+GROUP BY
+    YEAR(OrderDate)
+ORDER BY OrderYear;
