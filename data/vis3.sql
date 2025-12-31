01dadcbb5f4edeced338dfebda5b7645dfe52d91
iidaclaesson <iidaclaesson@gmail.com>
2025-12-31 12:52:09 +0100

    code to Q3

--- data/vis3.sql
+++ data/vis3.sql
@@ -2,3 +2,15 @@ USE AdventureWorks2025;
 
 
 SELECT * FROM Sales.SalesOrderHeader
+
+
+
+SELECT
+    SUM(TotalDue) AS TotalOrderSales,
+    YEAR(OrderDate) AS OrderYear,
+    MONTH(OrderDate) AS OrderMonth
+FROM Sales.SalesOrderHeader
+GROUP BY
+    YEAR(OrderDate),
+    MONTH(OrderDate)
+ORDER BY OrderYear , OrderMonth;
