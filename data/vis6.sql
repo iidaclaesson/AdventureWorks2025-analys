4eaad1f3a45371cb5a24e8652806e513d2d77af1
iidaclaesson <iidaclaesson@gmail.com>
2026-01-04 13:42:05 +0100

    sales per territory

---
+++ data/vis6.sql
@@ -0,0 +1,17 @@
+USE AdventureWorks2025;
+
+
+SELECT * FROM Sales.SalesTerritory
+SELECT * FROM Sales.SalesOrderHeader
+SELECT * FROM Sales.Customer
+
+
+SELECT
+    st.Name AS Territory,
+    SUM(soh.TotalDue) AS TotalSales,
+    COUNT(c.CustomerID) AS Customers
+FROM Sales.SalesTerritory st
+INNER JOIN Sales.SalesOrderHeader soh ON st.TerritoryID = soh.TerritoryID
+INNER JOIN Sales.Customer c ON soh.CustomerID = c.CustomerID
+GROUP BY st.Name
+ORDER BY TotalSales DESC;
