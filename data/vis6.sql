ff0a000e7577d03f7317489ce610639c9588d465
iidaclaesson <iidaclaesson@gmail.com>
2026-01-04 13:42:56 +0100

    fixed distinct customers

--- data/vis6.sql
+++ data/vis6.sql
@@ -9,7 +9,7 @@ SELECT * FROM Sales.Customer
 SELECT
     st.Name AS Territory,
     SUM(soh.TotalDue) AS TotalSales,
-    COUNT(c.CustomerID) AS Customers
+    COUNT(DISTINCT c.CustomerID) AS Customers
 FROM Sales.SalesTerritory st
 INNER JOIN Sales.SalesOrderHeader soh ON st.TerritoryID = soh.TerritoryID
 INNER JOIN Sales.Customer c ON soh.CustomerID = c.CustomerID
